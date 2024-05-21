78b566606ff8a129f48d2f4ea78c1601f205a63d
kavernic <133294831+kavernic@users.noreply.github.com>
2024-05-21 02:45:02 +0000

    ok

---
+++ Curso Pyton/Basico/hello.py
@@ -0,0 +1,25 @@
+# Esto es un comentario :Hola mundo
+print("Hola Python")
+print('Hola Python')
+
+
+"""
+Este es un
+comentario 
+en varias lineas
+
+"""
+
+''' 
+Este tambien es
+un comentario en
+varias lineas 
+
+'''
+# Consultar el tipo de dato
+print(type("Hola Python")) #Tipo 'str'
+print(type(5)) #Tipo 'int'
+print(type(1.5)) # Tipo 'float'
+print(type(True)) #Tipo'bool'
+
+ 
