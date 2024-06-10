0340c963e5c9eb4cd3758f6c9599b02dd738aa2a
Kavernic <ayselacaballero@gmail.com>
2024-06-10 18:39:03 -0400

    ajustes

---
+++ Curso Pyton/Ejercicios/D4/Ejercicios-D4.py
@@ -0,0 +1,36 @@
+# 1.Concatene la cadena 'Treinta', 'Días', 'De', 'Python' en una sola cadena, 'Treinta días de Python'.
+print('Treinta' + ' ' + 'Dias' + ' ' + 'De' + ' ' + 'Python')
+
+# 2. Concatene la cadena 'Codificación', 'Para', 'Todos' en una sola cadena, 'Codificación para todos'.
+# 3. Declare una variable denominada empresa y asígnele un valor inicial "Codificación para todos".
+# 4. Imprima la variable empresa usando print() .
+#Imprima la longitud de la cadena de la empresa utilizando el método len() e print() .
+#Cambie todos los caracteres a letras mayúsculas usando el método Upper() .
+#Cambie todos los caracteres a letras minúsculas usando el método lower() .
+#Utilice los métodos capitalize(), title(), swapcase() para formatear el valor de la cadena Coding For All .
+#3Corte (corte) la primera palabra de la cadena Coding For All .
+#Compruebe si la cadena Coding For All contiene una palabra Codificación utilizando el método index, find u otros métodos.
+#Reemplace la palabra codificación en la cadena 'Codificación para todos' por Python.
+#Cambie Python para todos a Python para todos usando el método de reemplazo u otros métodos.
+#Divida la cadena 'Codificación para todos' usando el espacio como separador (split()).
+#"Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon" divide la cadena por coma.
+#¿Cuál es el carácter en el índice 0 en la cadena Coding For All ?
+#¿Cuál es el último índice de la cadena Coding For All ?
+#Qué carácter está en el índice 10 en la cadena "Codificación para todos".
+#Cree un acrónimo o una abreviatura para el nombre 'Python para todos'.
+#Cree un acrónimo o una abreviatura para el nombre 'Coding For All'.
+#Utilice el índice para determinar la posición de la primera aparición de C en Coding For All.
+#Utilice el índice para determinar la posición de la primera aparición de F en Coding For All.
+#Utilice rfind para determinar la posición de la última aparición de l en Coding For All People.
+#Utilice index o find para encontrar la posición de la primera aparición de la palabra 'porque' en la siguiente oración: 'No se puede terminar una oración con porque porque porque es una conjunción'
+#Utilice rindex para encontrar la posición de la última aparición de la palabra porque en la siguiente oración: 'No puedes terminar una oración con porque porque porque es una conjunción'
+#Elimina la frase "porque porque porque" en la siguiente oración: "No puedes terminar una oración con porque porque porque es una conjunción".
+#Encuentra la posición de la primera aparición de la palabra 'porque' en la siguiente oración: 'No puedes terminar una oración con porque porque porque es una conjunción'
+#Elimina la frase "porque porque porque" en la siguiente oración: "No puedes terminar una oración con porque porque porque es una conjunción".
+#¿''Coding For All' comienza con una subcadena Codificación ?
+#¿'Codificación para todos' termina con una codificación de subcadena ?
+#'Coding For All', elimina los espacios finales izquierdo y derecho en la cadena dada.
+#¿Cuál de las siguientes variables devuelve True cuando usamos el método isidentifier()?
+# 30DíasDePython
+#treinta_días_de_python
+#La siguiente lista contiene los nombres de algunas de las bibliotecas de Python: ['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon']. Únase a la lista con un hash con una cadena de espacio.
