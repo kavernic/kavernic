df1fcaf205218f723d05a59f216db01df1f81cb8
Aysel Caballero <ayselacaballero@gmail.com>
2024-08-10 12:12:25 -0400

    go

---
+++ Curso Pyton/Basico/08_condicionals.py
@@ -0,0 +1,46 @@
+# Condicional 
+'''
+# syntax
+if condition:
+    this part of code runs for truthy conditions
+    
+'''
+
+'''
+Si la condición es verdadera, el primer bloque se ejecutará, si no se ejecutará la condición.
+
+# syntax
+if condition:
+    this part of code runs for truthy conditions
+else:
+    this part of code runs for false conditions
+
+'''
+
+'''
+if elif else 
+
+# syntax
+if condition:
+    code
+elif condition:
+    code
+else:
+    code
+
+
+
+'''
+
+'''
+Mano corta
+
+# syntax
+code if condition else code
+'''
+#**Ejemplo: **
+
+a = 3
+print('A is positive') if a > 0 else print('A is negative') # first condition met, 'A is positive' will be printed
+
+
